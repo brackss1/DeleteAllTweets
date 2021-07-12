6df7329aa7bc52d78ca103faf4041858108b5389
yuto ssk <yuto@yutonoMacBook-Air.local>
2021-07-12 19:14:16 +0900

    FinalProduct

---
+++ src.py
@@ -0,0 +1,15 @@
+import tweepy
+import os
+
+consumer_key = os.environ.get("CONSUMER_KEY")
+consumer_secret = os.environ.get("CONSUMER_SECRET")
+access_token = os.environ.get("ACCESS_TOKEN")
+access_secret = os.environ.get("ACCESS_SECRET")
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
+for status in tweepy.Cursor(api.user_timeline).items():
+    api.destroy_status(status.id)
